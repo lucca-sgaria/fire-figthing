f20e3c2fd7bcff1740623f8bd645b3cfd9e6f161
fabriciospark <fabricio@sparkag.com.br>
2019-07-01 14:53:00 -0300

    Atualização de parametros

--- src/br/com/ucs/firecombat/constants/MatrixConstants.java
+++ src/br/com/ucs/firecombat/constants/MatrixConstants.java
@@ -2,9 +2,9 @@ package br.com.ucs.firecombat.constants;
 
 public class MatrixConstants {
 
-	public static final int MATRIXSIZE = 5;
-	public static final int FIREAMOUNT = 1;
-	public static final int FIREFIGHTERSAMOUNT = 1;
+	public static final int MATRIXSIZE = 10;
+	public static final int FIREAMOUNT = 6;
+	public static final int FIREFIGHTERSAMOUNT = 3;
 	public static final int REFUGEE_AMOUNT = 1;
 	public static final int PARAMEDICS_AMOUNT = 1;
 
