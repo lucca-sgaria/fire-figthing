2eb27069d1a353184811639179d9aca36980eba3
fabriciospark <fabricio@sparkag.com.br>
2019-07-01 14:54:04 -0300

    Alterações layout

--- src/br/com/ucs/firecombat/constants/Params.java
+++ src/br/com/ucs/firecombat/constants/Params.java
@@ -4,7 +4,7 @@ public class Params {
 
     public static final int REFUGEE_LIFE = 5;
 
-
+    public static final boolean ENVI_PAINT_GRID = false;
     public static final String PATH_IMAGE_VICTIM = "resources/victim.jpg";
     public static final String PATH_IMAGE_REFUGEE = "resources/refugee.jpg";
     public static final String PATH_IMAGE_FIREFIGHTER = "resources/firefighter.jpg";
